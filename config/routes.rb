994ba2bd9de08b828efd7eeb50f91227a8f7fce1
muraleo <muraleo80923@gmail.com>
2018-11-11 11:24:22 -0500

    fix root routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'static_page/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_page/home'
+  root 'static_page#home'
 end
