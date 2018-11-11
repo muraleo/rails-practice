b0b5f77aa73a40b208eac90755fb9373f8d7b007
muraleo <muraleo80923@gmail.com>
2018-11-11 12:44:52 -0500

    add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_page#home'
+
   get 'static_page/about'
 
   get 'static_page/home'
 
   get 'static_page/help'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_page#home'
+  get 'static_page/contact'
+
 end
