cb20d5db97790b61cc7883c17c71b7a812842278
muraleo <muraleo80923@gmail.com>
2018-11-11 19:58:51 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_page#about'
   get  '/contact', to: 'static_page#contact'
   get  '/signup',  to: 'users#new'
+
+  resources :users
 end
